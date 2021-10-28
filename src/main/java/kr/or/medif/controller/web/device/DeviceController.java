66dab3a5f4565932624ac5190b6e20b11409c0a3
KimJaeil <jerrykim@limefriends.com>
2021-10-28 11:50:05 +0900

    fff

--- src/main/java/kr/or/medif/controller/web/device/DeviceController.java
+++ src/main/java/kr/or/medif/controller/web/device/DeviceController.java
@@ -1,6 +1,7 @@
 package kr.or.medif.controller.web.device;
 
 import kr.or.medif.common.Base;
+import kr.or.medif.domain.entity.Expert;
 import kr.or.medif.domain.vo.api.device.ApiVO;
 import kr.or.medif.domain.vo.api.device.RequestBody;
 import kr.or.medif.domain.vo.api.device.Form;
@@ -8,6 +9,9 @@ import kr.or.medif.service.ApiExplorer;
 import kr.or.medif.service.DeviceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -27,8 +31,9 @@ public class DeviceController extends Base {
 
     @RequestMapping("/device/list")
     public String expertList(Model model,
-                        @ModelAttribute Form form) throws IOException {
-        RequestBody objList = deviceService.test(form);
+                            @ModelAttribute Form form) {
+        model.addAttribute("form", form);
+        RequestBody objList = deviceService.getPrdLst(form);
         model.addAttribute("apiList", objList);
         model.addAttribute("mc", "device");
         return "/pages/device/deviceList";
--- src/main/java/kr/or/medif/service/DeviceService.java
+++ src/main/java/kr/or/medif/service/DeviceService.java
@@ -1,11 +1,15 @@
 package kr.or.medif.service;
 
 import kr.or.medif.common.Base;
+import kr.or.medif.common.Constants;
 import kr.or.medif.common.retrofit.ServerAPIs;
 import kr.or.medif.domain.vo.api.device.ApiVO;
 import kr.or.medif.domain.vo.api.device.Form;
 import kr.or.medif.domain.vo.api.device.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -17,11 +21,11 @@ public class DeviceService extends Base {
     @Autowired
     private ServerAPIs serverAPIs;
 
-    public RequestBody test(Form form) throws IOException {
+    public RequestBody getPrdLst(Form form)  {
         if(form.getPageNo() == null) {
             form.setPageNo(1);
         }
-        Call<ApiVO> call = serverAPIs.getDeviceData(form.getServiceKey(), 1, form.getNumOfRows() ,form.getType() ,form.getUDIDI_CD(), "치과");
+        Call<ApiVO> call = serverAPIs.getDeviceData(form.getServiceKey(), form.getPageNo(), form.getNumOfRows() ,form.getType() ,form.getUDIDI_CD(), form.getPRDLST_NM());
         try {
             Response<ApiVO> response = call.execute();
             if(response.isSuccessful()) {
